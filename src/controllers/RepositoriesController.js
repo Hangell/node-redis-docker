f08ed89252e5991b1672fcc2a4b11bf339e60d6b
Rodrigo Rangel <rodrigo@hangell.org>
2023-02-02 20:03:07 -0300

    Node in Docker

--- src/controllers/RepositoriesController.js
+++ src/controllers/RepositoriesController.js
@@ -1,11 +1,36 @@
 const githubAPI = require('../services/githubApi');
 
 async function listAllRepositoriesByUser(req, res) {
-    const {users} = req.params;
+    try {
+        const { users } = req.params;
 
-    const {data} = await githubAPI.get(`/users/${users}/repos`);
+        const { data } = await githubAPI.get(`/users/${users}/repos`);
 
-    return res.status(200).json(data);
+        const { id, login, avatar_url } = data[0].owner
+        const userData = { id, login, avatar_url };
+
+        const formattedRepositoriesData = [];
+
+        for (const repos of data) {
+            formattedRepositoriesData.push({
+                id: repos.id,
+                name: repos.name,
+                private: repos.private,
+                url: repos.url,
+                created_at: repos.created_at,
+                language: repos.language,
+            });
+        }
+
+        return res.status(200).json({
+            user: userData,
+            repos: formattedRepositoriesData
+        });
+    } catch (error) {
+        return res.status(400).json({
+            message: "Failed to list all repositories!"
+        })
+    }
 }
 
 module.exports = { listAllRepositoriesByUser };
